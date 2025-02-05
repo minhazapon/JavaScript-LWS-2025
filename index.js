41ae85c4d486732652c0d1df10d06037866d45e9
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-04 20:18:53 -0800

    added file

--- index.js
+++ index.js
@@ -3,4 +3,60 @@ const name = "minhazul abedin apon";
 const number = 1950374409;
 const allInfo = { name, number }
 
-console.log(allInfo) 
+console.log(allInfo)
+
+//javaScript string
+
+const A = 'bangladesh';
+const B = 'india';
+const C = 'pakistan';
+const D = 'afganistan';
+const E = 'bhutan'
+const allNameZ = { A, B, C, D, E }
+console.log(allNameZ)
+
+//javaScript Number
+
+const num1 = 12344;
+const num2 = 234545;
+const num3 = 6764535;
+
+const allNums = num1 + num2 + num3
+
+const allN = allNums + 1000
+
+const numbers = allN / 5
+
+console.log(numbers)
+
+
+//javaScript boolean 
+
+const isAponPythonDeveloper = false
+const isAponJavaScriptDeveloper = true
+console.log(isAponJavaScriptDeveloper, isAponPythonDeveloper)
+
+
+//javaScript array 
+
+const allArr = [23, 3545, 456, 66, 245, 66]
+
+const array = allArr + 10000
+
+console.log(array)
+
+//javaScript object 
+
+const aponInformation = {
+
+    name: 'minhazul abedin apon',
+    number: 1950374409,
+    profession: 'javaScript Engineer',
+    LifeType: 'osohay'
+
+}
+
+console.log(aponInformation.profession)
+console.log(aponInformation.number)
+console.log(aponInformation.name)
+console.log(aponInformation.LifeType)
