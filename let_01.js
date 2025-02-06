3cf4687a0488ce982f084b934cce27b935ec77ae
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-05 19:25:16 -0800

    added let keyword

---
+++ let_01.js
@@ -0,0 +1,22 @@
+
+
+let name1 = "minhazul abedin apon"
+console.log(name1)
+
+let number = 1950374409
+console.log(number)
+
+let isJavaScriptDeveloperApon = true
+console.log(isJavaScriptDeveloperApon)
+
+let OneMan = (a, b) => {
+
+    let all = a + b
+    let X = 10 + all
+    console.log(X)
+
+}
+
+OneMan(12, 20)
+
+
